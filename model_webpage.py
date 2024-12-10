2ef2412bece7ca227ffb9fe4c2bb9e107c059689
Jackson Cmelak <jackowen@gmail.com>
2024-12-10 15:21:33 -0800

    Update model_webpage.py

--- model_webpage.py
+++ model_webpage.py
@@ -1,68 +1,57 @@
 import streamlit as st
-import tensorflow as tf
-import numpy as np
-
-
-def load_model():
-    model = tf.keras.models.load_model('best_model.h5')
-    return model 
-
-model = load_model()
 
 st.write("""
          # NFL Victor Predictor
          """)
 
 team_list = ['Select Team',
-    'bears',
-    'bengals',
-    'bills',
-    'broncos_data',
-    'browns_data',
-    'buccaneers',
-    'cardinals',
-    'chargers',
-    'chiefs',
-    'colts',
-    'commanders',
-    'cowboys',
-    'dolphins',
-    'eagles',
-    'falcons',
-    'giants',
-    'jaguars',
-    'jets',
-    'lions',
-    'packers',
-    'panthers',
-    'patriots',
-    'raiders',
-    'rams',
-    'ravens',
-    'saints',
-    'seahawks',
-    'steelers',
-    'texans',
-    'titans',
-    'vikings',
+    'Bears',
+    'Bengals',
+    'Bills',
+    'Broncos',
+    'Browns',
+    'Buccaneers',
+    'Cardinals',
+    'Chargers',
+    'Chiefs',
+    'Colts',
+    'Commanders',
+    'Cowboys',
+    'Dolphins',
+    'Eagles',
+    'Falcons',
+    'Giants',
+    'Jaguars',
+    'Jets',
+    'Lions',
+    'Packers',
+    'Panthers',
+    'Patriots',
+    'Raiders',
+    'Rams',
+    'Ravens',
+    'Saints',
+    'Seahawks',
+    'Steelers',
+    'Texans',
+    'Titans',
+    'Vikings',
     '49ers']
 
-team_1 = st.selectbox("Team 1", team_list)
-team_2 = st.selectbox("Team 2", team_list)
-
-#def import_and_predict(image_data, model):
-
-#    prediction = model.predict()
-
-#    return prediction
+home_team = st.selectbox("Home Team", team_list)
+away_team = st.selectbox("Away Team", team_list)
 
 if st.button("Predict", type="primary"):
-    if team_1 == team_2:
-        st.text("A team can't play against itself!")
-    elif (team_1 == 'Select Team') or (team_2 == 'Select Team'):
+    
+    if (home_team == 'Select Team') or (away_team == 'Select Team'):
         st.text("Please select two different teams to compete")
+    elif home_team == away_team:
+        st.text("A team can't play against itself")
     else:
         output = "This is a placeholder"
         st.success(output)
+    
+
+
     
 
